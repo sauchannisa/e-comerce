853fbdaac3d16aa67c493d072b53872690c19839
sauchannisa <sauchinnisa@gmail.com>
2024-10-19 09:15:06 +0700

    Finish TP1 wih Vue Js
    and more ...

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  
+This is my fisrt Vue JS project.
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
