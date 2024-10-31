cd3537e28e3189af39057f40f937b8c4906ae62f
sauchannisa <sauchinnisa@gmail.com>
2024-10-31 09:11:21 +0700

    TP2

--- src/components/category.vue
+++ src/components/category.vue
@@ -4,7 +4,7 @@
         <img class="mx-auto h-450 w-450 square-full" :src="category.imageUrl" alt="" />
             <p class="text-sm leading-2 mt-5 text-gray-800">{{ category.name }}</p>
             <ul role="list" class="mt-2 flex justify-center gap-x-1">
-              <span style="text-decoration: color(rgba(182, 182, 182, 1));">{{ category.quantity }}</span> Items
+              <span style="text-decoration: color(rgba(182, 182, 182, 1));">{{ category.productCount }}</span> Items
             </ul>
           
         </div> 
