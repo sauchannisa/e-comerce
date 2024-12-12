7bef1af88ad53ec63da9ed42550d0e3177048222
sauchannisa <sauchinnisa@gmail.com>
2024-12-12 11:04:49 +0700

    TP5 first submit

---
+++ Documents/GIC.2024/nestedroute/src/components/Page3.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="page3">
+      <p>Welcome to Page3</p>
+    </div>
+  </template>
+  <script >
+  
+  export default{
+      name:"page1",
+  }
+  
+  </script>
+  
+  
+  <style scoped>
+.page3 {
+  width: 90vw;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(0, 255, 153);
+}</style>
+  
